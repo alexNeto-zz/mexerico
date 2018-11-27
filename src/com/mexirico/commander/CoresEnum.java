d220b879a7e82ce1258cbe4a9e978b3c55c8d113
alexNeto <alex.neto3@hotmail.com>
2018-11-27 18:41:58 -0200

    adicionado método para colorir texto

---
+++ src/com/mexirico/commander/CoresEnum.java
@@ -0,0 +1,19 @@
+package com.mexirico.commander;
+
+public enum CoresEnum {
+
+	VERMELHO("\u001B[31m"), 
+	VERDE("\u001B[32m"), 
+	AMARELO("\u001B[33m"), 
+	AZUL("\u001B[34m"),
+	ROXO("\u001B[35m"),
+	ANIL("\u001B[36m"), 
+	BRANCO("\u001B[37m");
+
+	public String cor;
+
+	CoresEnum(String cor) {
+		this.cor = cor;
+	}
+
+}
